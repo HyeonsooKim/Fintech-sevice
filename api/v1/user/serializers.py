d2827239f4f2448fe6008a3d5ff6de8e12049518
HyeonsooKim <gustn9442@naver.com>
2023-02-22 15:37:04 +0900

    refactor: User Serializer, View 간소화

--- api/v1/user/serializers.py
+++ api/v1/user/serializers.py
@@ -7,7 +7,7 @@ from datetime import date
 
 User = get_user_model()
 
-class SignUpSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         required=True,
         write_only=True,
@@ -16,7 +16,7 @@ class SignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'name']
+        fields = "__all__"
 
     def create(self, validated_data):
         password = validated_data.get('password')
@@ -37,46 +37,5 @@ class SignUpSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Username already exists")
 
         data['date_joined'] = date.today()
-
-        return data
-
-
-class SignInSerializers(serializers.ModelSerializer):
-    username = serializers.CharField(
-        required=True,
-        write_only=True,
-    )
-
-    password = serializers.CharField(
-        required=True,
-        write_only=True,
-        style={'input_type': 'password'}
-    )
-
-    class Meta(object):
-        model = User
-        fields = ['username', 'password']
-
-    def validate(self, data):
-        username = data.get('username', None)
-        password = data.get('password', None)
-
-        if User.objects.filter(username=username).exists():
-            user = User.objects.get(username=username)
-
-            if not user.check_password(password):
-                raise serializers.ValidationError("Wrong password")
-        else:
-            raise serializers.ValidationError("User account not exist")
-
-        token = RefreshToken.for_user(user)
-        refresh = str(token)
-        access = str(token.access_token)
-
-        data = {
-            'user': username,
-            'refresh': refresh,
-            'access': access,
-        }
 
         return data
